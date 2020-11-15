7a21028aa58dc27a55f19ff05df29c520f1434a4
AcidGo <zhangruijun95@163.com>
2020-11-15 19:28:11 +0800

    add UserParameter conflict deal

--- zbx_agent2upgrade.py
+++ zbx_agent2upgrade.py
@@ -7,10 +7,15 @@
 
 
 from __future__ import print_function
-import platform, sys, os, time
+import platform, glob, os, sys, time
 import logging
+import shutil
 import re
 import subprocess
+
+from collections import OrderedDict
+from ConfigParser import RawConfigParser
+from StringIO import StringIO
 
 
 # CONFIG
@@ -21,6 +26,46 @@ AGENTD_CONF = "/etc/zabbix/zabbix_agentd.conf"
 CONF_IGNORE_ITEM = [
     "PidFile",
     "StartAgents"
+]
+# from https://www.zabbix.com/documentation/current/manual/config/items/plugins (5.2)
+CONF_CONFLICT_UP = [
+    "agent.hostname", "agent.ping", "agent.version", 
+    "ceph.df.details", "ceph.osd.stats", "ceph.osd.discovery", "ceph.osd.dump", "ceph.ping", "ceph.pool.discovery", "ceph.status", 
+    "system.cpu.discovery", "system.cpu.num", "system.cpu.util", "docker.container_info", "docker.container_stats", "docker.containers", 
+    "docker.containers.discovery", "docker.data_usage", "docker.images", "docker.images.discovery", "docker.info", "docker.ping", 
+    "vfs.file.cksum", "vfs.file.contents", "vfs.file.exists", "vfs.file.md5sum", "vfs.file.regexp", "vfs.file.regmatch", "vfs.file.size", "vfs.file.time", 
+    "kernel.maxfiles", "kernel.maxproc", 
+    "log", "log.count", "logrt", "logrt.count", 
+    "memcached.ping", "memchached.stats", 
+    "modbus.get", 
+    "mqtt.get", 
+    "mysql.db.discovery", "mysql.db.size", "mysql.get_status_variables", "mysql.ping", "mysql.replication.discovery", "mysql.replication.get_slave_status", "mysql.version", 
+    "net.if.collisions", "net.if.discovery", "net.if.in", "net.if.out", "net.if.total", 
+    "oracle.diskgroups.stats", "oracle.diskgroups.discovery", "oracle.archive.info", "oracle.archive.discovery", "oracle.cdb.info", "oracle.custom.query", 
+    "oracle.datafiles.stats", "oracle.db.discovery", "oracle.fra.stats", "oracle.instance.info", "oracle.pdb.info", "oracle.pdb.discovery", 
+    "oracle.pga.stats", "oracle.ping", "oracle.proc.stats", "oracle.redolog.info", "oracle.sga.stats", "oracle.sessions.stats", "oracle.sys.metrics", 
+    "oracle.sys.params", "oracle.ts.stats", "oracle.ts.discovery", "oracle.user.info", 
+    "pgsql.ping", "pgsql.db.discovery", "pgsql.db.size", "pgsql.db.age", "pgsql.database.bloating_tables", "pgsql.replication_lag.sec", "pgsql.replication_lag.b", 
+    "pgsql.replication.count", "pgsql.replication.status", "pgsql.replication.recovery_role", "pgsql.cache.hit", "pgsql.connections", "pgsql.archive", 
+    "pgsql.bgwriter", "pgsql.dbstat.sum", "pgsql.dbstat", "pgsql.wal.stat", "pgsql.locks", "pgsql.pgsql.oldest.xid", "pgsql.uptime", 
+    "proc.cpu.util", 
+    "redis.config", "redis.info", "redis.ping", "redis.slowlog.count", 
+    "system.swap.size", "system.run", "systemd.unit.discovery", "systemd.unit.info", 
+    "net.tcp.port", "net.udp.service", "net.udp.service.perf", 
+    "system.hostname", "system.sw.arch", "system.uname", "system.uptime", 
+    "vfs.dev.discovery", "vfs.dev.read", "vfs.dev.write", 
+    "web.page.get", "web.page.perf", "web.page.regexp", 
+    "net.tcp.listen", "net.udp.listen", 
+    "sensor", 
+    "system.boottime", "system.cpu.intr", "system.cpu.load", "system.cpu.switches", "system.hw.cpu", "system.hw.macaddr", 
+    "system.localtime", "system.sw.os", "system.swap.in", "system.swap.out", 
+    "vfs.fs.discovery", 
+    "zabbix.stats", 
+    "net.dns", "net.dns.record", "net.tcp.service", "net.tcp.service.perf", 
+    "proc.mem", "proc.num", 
+    "system.hw.chassis", "system.hw.devices", "system.sw.packages", "system.users.num", 
+    "vfs.dir.count", "vfs.dir.size", "vfs.fs.get", "vfs.fs.inode", "vfs.fs.size", 
+    "vm.memory.size", 
 ]
 # EOF CONFIG
 
@@ -83,7 +128,7 @@ def lnx_command_execute(command_lst):
     logging.info("-"*30)
     return True
 
-def parse_zbx_conf(path):
+def parse_zbx_conf(path, is_multi=False):
     """分析 zabbix-agentd 和 zabbix-agent 等的 conf 文件，转为换列表。
 
     Args:
@@ -91,15 +136,81 @@ def parse_zbx_conf(path):
     Returns:
         <list>: 配置文件的键值对。
     """
-    from ConfigParser import RawConfigParser
-    from StringIO import StringIO
-
-    cp = RawConfigParser()
+    cp = RawConfigParser() if not is_multi else RawConfigParser(dict_type=MultiOrderedDict)
     cp.optionxform = str
     with open(path, "r") as f:
         stream = StringIO("[dummy_section]\n" + f.read())
         cp.readfp(stream)
     return cp.items("dummy_section")
+
+def check_conflict_up(agent_conf_path):
+    """检查是否存在有与 Zabbix-Agent2 内置的 key 冲突的 UP。
+
+    Args:
+        agent_conf_path: Zabbix-Agentd/2 的配置文件路径。
+    Returns:
+        <dict>: {<up_path>: [<key1>, <key2> ...]}
+    """
+    res = {}
+    self_key = "@{!s}".format(agent_conf_path)
+    for i in parse_zbx_conf(agent_conf_path):
+        if i[0] == "UserParameter":
+            if self_key not in res:
+                res[self_key] = [i[1].split(",")[0].strip()]
+            else:
+                res[self_key].append(i[1].split(",")[0].strip())
+        if i[0] == "Include":
+            for j in glob.glob(i[1]):
+                res[j] = []
+
+    for k in res:
+        if k == self_key:
+            continue
+        up_items = parse_zbx_conf(k, True)
+        for up in up_items:
+            if up[0] != "UserParameter":
+                continue
+            for entry in up[1].split("\n"):
+                res[k].append(entry.split(",")[0].strip())
+
+    for v in res.values():
+        for v_i in v:
+            if v_i in CONF_CONFLICT_UP:
+                return True, res
+
+    return False, res
+
+def deal_conflict_up(up_dict):
+    """
+    """
+    for k, v in up_dict.items():
+        has_deal = False
+        for v_i in v:
+            if has_deal:
+                break
+            if v_i in CONF_CONFLICT_UP:
+                if k.startswith("@"):
+                    conf_path = k.split("@")[-1].strip()
+                    context_list = []
+                    with open(conf_path, "r") as f:
+                        original_list = [l for l in f]
+                    for line in original_list:
+                        if re.search(r"^\s*UserParameter\s*=\s*{!s}\s*,.*?$".format(v_i), line):
+                            logging.info("deal with conflict UserParameter on self config: {!s}".format(line.strip()))
+                            continue
+                        context_list.append(line)
+                    with open(conf_path, "w") as f:
+                        for line in context_list:
+                            f.write(line)
+                else:
+                    # 直接整个文件关闭匹配
+                    conf_dir = os.path.dirname(k)
+                    conf_name = os.path.basename(k)
+                    disable_path = os.path.join(conf_dir, conf_name + ".disable")
+                    shutil.move(k, disable_path)
+                    logging.info("deal with conflict UserParameter on outer config: {!s} -> {!s}".format(k, disable_path))
+                    has_deal = True
+                    break
 
 def update_diff_conf(path, update_items, add_items, ignore_items):
     """将差异的 conf 文件内容补齐。
@@ -163,7 +274,7 @@ def update_diff_conf(path, update_items, add_items, ignore_items):
 def systemctl_action(action, service):
     """
     """
-    if action not in ("start", "stop", "restart", "enable", "disable"):
+    if action not in ("start", "stop", "restart", "status", "enable", "disable"):
         raise Exception("not support the systemctl action: {!s}".format(action))
     command_lst = ["systemctl", action, service]
     if lnx_command_execute(command_lst):
@@ -222,7 +333,7 @@ def install_agent2_rpm(url):
         logging.error("zabbix-agent2 rpm installing is failed")
         raise Exception()
 
-def conv_agent2_conf(agentd_conf_path, agent2_conf_path):
+def conv_agent2_conf(agentd_conf_path, agent2_conf_path, is_force):
     """对齐存在的 agentd 的配置。
     """
     agentd_items = parse_zbx_conf(agentd_conf_path)
@@ -252,6 +363,21 @@ def conv_agent2_conf(agentd_conf_path, agent2_conf_path):
     logging.debug("in conv_agent2_conf, add_items: {!s}".format(str(add_items)))
     update_diff_conf(AGENT2_CONF, update_items, add_items, CONF_IGNORE_ITEM)
 
+    has_conflict, up_dict = check_conflict_up(AGENT2_CONF)
+
+    logging.debug("="*10 + " all UserParameter:")
+    for i in up_dict:
+        logging.debug("{!s}:".format(i))
+        for j in up_dict[i]:
+            logging.debug("\t{!s}".format(j))
+    logging.debug("="*10 + " EOF all UserParameter")
+
+    if has_conflict:
+        logging.warning("found conflict UserParameter in the config")
+        if not is_force:
+            raise Exception("not force, exit the progress")
+        deal_conflict_up(up_dict)
+
 def conv_agent2_enable():
     """
     """
@@ -267,31 +393,48 @@ def conv_agent2_enable():
     if not systemctl_action("enable", "zabbix-agent2"):
         logging.error("systemctl enable zabbix-agent2 is failed, please check")
         return False
+    if not systemctl_action("status", "zabbix-agent2"):
+        logging.error("systemctl enable zabbix-agent2 is failed, please check")
+        return False
+
     return True
 
-def execute(url):
+def execute(url, is_force):
     # 1. 抓取一次当前 agentd 的版本，备份 agentd 的文件。
     upgrade_pre()
     # 2. yum/rpm 安装对应的 agent2 rpm。
     install_agent2_rpm(url)
     # 3. 根据现有的 agentd 的配置填充到 agent2 中。
-    conv_agent2_conf(AGENTD_CONF, AGENT2_CONF)
+    conv_agent2_conf(AGENTD_CONF, AGENT2_CONF, is_force)
     # 4. systemctl stop zabbix-agent 或 service zabbix-agent stop。（这里最好 rhel7 的才升级）
     # systemctl disable zabbix-agent
     # systemctl start zabbix-agent2
     # systemctl enable zabbix-agent2
+    # systemctl status zabbix-agent2
     conv_agent2_enable()
+
+class MultiOrderedDict(OrderedDict):
+    """from https://stackoverflow.com/questions/15848674/how-to-configparse-a-file-keeping-multiple-values-for-identical-keys
+    """
+    def __setitem__(self, key, value):
+        if isinstance(value, list) and key in self:
+            self[key].extend(value)
+        else:
+            super(MultiOrderedDict, self).__setitem__(key, value)
+            # super().__setitem__(key, value) in Python 3
 
 
 if __name__ == "__main__":
     # ########## Self Test
     # INPUT_AGENT2_RPM_URL = "http://192.168.66.180:8080/zabbix-agent2-5.0.1-1.el7.x86_64.rpm"
+    # INPUT_IS_FORCE = False
     # ########## EOF Self Tes
 
     init_logger("debug")
     try:
         execute(
-            url = INPUT_AGENT2_RPM_URL
+            url = INPUT_AGENT2_RPM_URL,
+            is_force = INPUT_IS_FORCE
         )
     except Exception as e:
         logging.exception(e)
